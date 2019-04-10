2129cf21b47b5d56a4c497755c88d201dcb53d17
Xiaotao Shen <shenxt1990@163.com>
2019-04-10 14:35:29 -0700

    Add manu for sxtScale

--- R/sxtScale.R
+++ R/sxtScale.R
@@ -1,22 +1,32 @@
-sxtScale <- function(sample,
+#' @title sxtScale
+#' @description Scale data (matirx or data frame)
+#' @author Xiaotao Shen
+#' \email{shenxt1990@@163.com}
+#' @param data A matarix or data frame, rows represents variables, and columns represents observations.
+#' @param method Scale method. no, pareto or auto.
+#' @param center Center data or not, default is TRUE.
+#' @return A scaled data.
+#' @export
+
+sxtScale <- function(data,
                      method = c("no", "pareto", "auto"),
                      center = TRUE){
   method <- match.arg(method)
 
   if(method == 'pareto') {
-    if(center == TRUE) sample <- t(apply(sample, 1, function(x) (x-mean(x))/sqrt(sd(x))))
-    if(center == FALSE) sample <- t(apply(sample, 1, function(x) x/sqrt(sd(x))))
+    if(center == TRUE) data <- t(apply(data, 1, function(x) (x-mean(x))/sqrt(sd(x))))
+    if(center == FALSE) data <- t(apply(data, 1, function(x) x/sqrt(sd(x))))
   }
 
   if(method == 'auto') {
-    if(center == TRUE) sample <- t(apply(sample, 1, function(x) (x-mean(x))/sd(x)))
-    if(center == FALSE) sample <- t(apply(sample, 1, function(x) x/sd(x)))
+    if(center == TRUE) data <- t(apply(data, 1, function(x) (x-mean(x))/sd(x)))
+    if(center == FALSE) data <- t(apply(data, 1, function(x) x/sd(x)))
   }
 
   if(method == 'no') {
-    sample <- sample
+    data <- data
   }
 
-  sample
+  data
 
 }
