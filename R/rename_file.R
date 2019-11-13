9c7fc670f0d29b2ce605a19c7c4892c4eb275fc4
Xiaotao Shen <shenxt1990@163.com>
2019-11-13 13:20:14 -0800

    rename

---
+++ R/rename_file.R
@@ -0,0 +1,21 @@
+# setwd("F:/PS4P/HILIC/POS/batch3")
+# file <- dir()
+# library(tidyverse)
+# file <- grep(pattern = "2019", file, value = TRUE)
+# file2 <- file %>%
+#   stringr::str_replace("_2019[0-9]{3,18}", "")
+#
+# file.rename(from = file, to = file2)
+#
+#
+# setwd("F:/PS4P/HILIC/NEG/batch3")
+# file <- dir()
+# library(tidyverse)
+# file <- grep(pattern = "2019", file, value = TRUE)
+# file2 <- file %>%
+#   stringr::str_replace("_2019[0-9]{3,18}", "")
+#
+# file.rename(from = file, to = file2)
+#
+#
+#
